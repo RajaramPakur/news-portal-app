8ad2479eb36e87a3dbe57cb838f831000da0ea96
rajarampakur <pakurrajaram@gmail.com>
2020-06-14 22:42:48 +0545

    Updates

---
+++ precache-manifest.2b4de2fc54e1c659351e178c3dbe2309.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "693067b5bba46a6cb733f3c815f1ac59",
+    "url": "/news-portal-app/index.html"
+  },
+  {
+    "revision": "ebb19f67e9e181e46568",
+    "url": "/news-portal-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b811af3689eb06c5aea",
+    "url": "/news-portal-app/static/js/2.0663cec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/news-portal-app/static/js/2.0663cec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebb19f67e9e181e46568",
+    "url": "/news-portal-app/static/js/main.dcb5eb34.chunk.js"
+  },
+  {
+    "revision": "da9d1e1eeccb76ae6657",
+    "url": "/news-portal-app/static/js/runtime-main.bfe9adc0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/news-portal-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
